2d2032bc9b18e65dfa33f2a040ebc02dd69d8d09
Anderson Chong <andersonchong19742001@gmail.com>
2020-12-16 21:36:08 +0800

    New Currency Class
    Added
    1. Method to initialize API connection (auto refresh connection every 1 hour and update currency exhange rate)
    2. Methods to manage main currency (update, view)
    3. Methods to manage sub-currencies (add, delete, view)
    4. Method for currency conversion
    5. Method to retrieve latest currency exchange rate (based on existing user-selected-sub-currencies)
    
    Modified
    1. added http package in pubspec.yaml
    2. initialize connection during startup in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:budget_tracking_system/pages/mainpage.dart';
 import 'package:budget_tracking_system/pages/login.dart';
+import 'package:budget_tracking_system/services/currency.dart';
 
-void main() => runApp(MaterialApp(
-  initialRoute: '/login',
-  routes: {
-    '/login': (context) => Login(),
-    '/mainpage': (context) => Mainpage()
-  },
-));
-
+void main() async {
+  Currency.init();
+  runApp(MaterialApp(
+    initialRoute: '/login',
+    routes: {
+      '/login': (context) => Login(),
+      '/mainpage': (context) => Mainpage()
+    },
+  ));
+}

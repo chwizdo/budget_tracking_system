f88abc9cd1cf68ceb4dc9bf28ef754ba5ffbf62a
Anderson Chong <andersonchong19742001@gmail.com>
2020-12-21 18:34:21 +0800

    Added New Account, Category and Record Classes
    Added
    1. create, update, calculation functionalities for Account class
    2. create, update, functionalities for Category class
    3. create, update, functionalities for Record class
    
    Modified
    1. Added meta dependency in pubspec.yaml
    
    TODO
    1. Database integration

---
+++ lib/services/account.dart
@@ -0,0 +1,65 @@
+import 'package:meta/meta.dart';
+
+/*
+  Add ✅
+  Update
+  Calculation ✅
+  Delete
+  View
+*/
+
+class Account {
+  String _name;
+  final _currency;
+  double _amount;
+
+  // constructor to create new account
+  Account({
+    @required String name,
+    @required String currency,
+    double amount = 0,
+  })  : _name = name,
+        _currency = currency,
+        _amount = amount;
+
+  // add or reduce amount in account
+  // return amount after modification
+  double modAmount({double amount}) {
+    _amount += amount;
+    return _amount;
+  }
+
+  // retrieve account name
+  String get name {
+    return _name;
+  }
+
+  // retrieve account currency
+  String get currency {
+    return _currency;
+  }
+
+  // retrieve account amount
+  double get amount {
+    return _amount;
+  }
+
+  // Update account properties
+  // user are not allowed to change currency
+  // TODO return difference in amount (Need to add new record for settings changes)
+  double setProperties({
+    @required String name,
+    double amount = 0,
+  }) {
+    var difference = amount - _amount;
+    _name = name;
+    _amount = amount;
+    return difference;
+  }
+
+  // TODO delete account
+  void rmAccount() {}
+
+  // TODO retrieve all accounts in database
+  static List<Account> getAccounts() {}
+}

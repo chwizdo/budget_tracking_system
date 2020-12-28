016503447114a78245c14e92bb41051888e25f47
Anderson Chong <andersonchong19742001@gmail.com>
2020-12-28 18:32:01 +0800

    Added Search Method to Record Service
    Added / Modified
    1. Search functionality added
    2. Added 'account' property to be added into database
    
    TODO
    1. Change category and account into map
    2. retrieve auto generated record id and update into database
    3. Set up data retrieval from database and update into local memory
    4. Add data to local memory first before adding to database
    5. Update data type of category and account to be its own class

--- lib/services/record.dart
+++ lib/services/record.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meta/meta.dart';
 // import 'package:budget_tracking_system/services/account.dart';
 // import 'package:budget_tracking_system/services/category.dart';
@@ -21,9 +20,10 @@ class Record {
   String _note;
   String _attachment;
   bool _isFav;
+  static final List<Record> _list = [];
 
   // constructor to create new record
-  // type, date & time, category and amount are mandatory
+  // uid, type, date & time, category, account and amount are mandatory
   Record({
     @required String uid,
     @required String type,
@@ -45,19 +45,18 @@ class Record {
         _note = note,
         _attachment = attachment,
         _isFav = isFav {
-    //retrieve specfic user
-    //var user = FirebaseAuth.instance.currentUser;
-    //Firestore.instance.collection("users").document(user.toString()).get();
-    //add
+    //add data to database
     Firestore.instance
         .collection("users")
         .document(_uid)
         .collection("record")
         .add({
+      'id': '',
       'type': _type,
       'title': _title,
       'datetime': _dateTime,
-      'category': _category,
+      'category': _category, // TODO make it into a map
+      'account': _account, // TODO make it into a map
       'amount': _amount,
       'note': _note,
       'attachment': _attachment,
@@ -102,6 +101,10 @@ class Record {
     return _isFav;
   }
 
+  static List<Record> get list {
+    return _list;
+  }
+
   // update all private properties
   // type, date & time, category and amount are mandatory
   void setProperties({
@@ -111,9 +114,9 @@ class Record {
     @required String category,
     @required String account,
     @required double amount,
-    String note,
-    String attachment,
-    bool isFav,
+    String note = '',
+    String attachment = '',
+    bool isFav = false,
   }) {
     _type = type;
     _title = title;
@@ -126,7 +129,120 @@ class Record {
     _isFav = isFav;
   }
 
-  static List<Record> search() {}
+  static List<Record> search({
+    String type = '',
+    String title = '',
+    DateTime startDate,
+    DateTime endDate,
+    String category,
+    String account,
+    double leastAmount,
+    double maxAmount,
+    String note = '',
+    bool attachment = false,
+    // bool isFav,
+  }) {
+    List<Record> list = [];
+    _list.forEach(
+      (value) {
+        // variables for validation
+        var isMatch = 0;
+        var hasValue = false;
+
+        if (type.isNotEmpty) {
+          hasValue = true;
+          isMatch++;
+          if (value.type == type) {
+            isMatch--;
+          }
+        }
+
+        if (title.isNotEmpty) {
+          hasValue = true;
+          isMatch++;
+          if (value.title.toLowerCase().contains(title.toLowerCase())) {
+            isMatch--;
+          }
+        }
+
+        if (startDate != null) {
+          hasValue = true;
+          isMatch++;
+          if (!value.dateTime.isBefore(startDate)) {
+            isMatch--;
+          }
+        }
+
+        if (endDate != null) {
+          hasValue = true;
+          isMatch++;
+          if (!value.dateTime.isAfter(endDate)) {
+            isMatch--;
+          }
+        }
+
+        if (category != null) {
+          hasValue = true;
+          isMatch++;
+          // TODO add .name on both sides
+          // TODO change to category id
+          if (value.category == category) {
+            isMatch--;
+          }
+        }
+
+        if (account != null) {
+          hasValue = true;
+          isMatch++;
+          // TODO add .name on both sides
+          // TODO change to account id
+          if (value.account == account) {
+            isMatch--;
+          }
+        }
+
+        if (leastAmount != null) {
+          hasValue = true;
+          isMatch++;
+          if (value.amount >= leastAmount) {
+            isMatch--;
+          }
+        }
+
+        if (maxAmount != null) {
+          hasValue = true;
+          isMatch++;
+          if (value.amount <= maxAmount) {
+            isMatch--;
+          }
+        }
+
+        if (note.isNotEmpty) {
+          hasValue = true;
+          isMatch++;
+          if (value.note.toLowerCase().contains(note.toLowerCase())) {
+            isMatch--;
+          }
+        }
+
+        if (attachment == true) {
+          hasValue = true;
+          isMatch++;
+          if (value.attachment.isNotEmpty) {
+            isMatch--;
+          }
+        }
+
+        // if isMatch is more than 1, there are some field that does not match
+        // if hasValue is false, none of the if statements evaluates to true, means that no valued arguments are passed
+        if (isMatch == 0 && hasValue) {
+          list.add(value);
+        }
+      },
+    );
+    // return the list of records that matched the criteria
+    return list;
+  }
 
   // TODO delete record
   // Implemented in main page
